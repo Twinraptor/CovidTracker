6efa192428547f9c8c5e553c94d106e011855433
twinraptor <mythosx@hotmail.com>
2021-02-04 13:03:23 -0800

    labled all the divs. committing to do fresh work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="headerTitle">
         Covid 19 Tracker
+      </div>
+      <div className="dropdown">
+        Drop down
+      </div>
+      <div className="main">
+        <div className="databoxes">
+          <div className="databox">
+            Databox 1
+          </div>
+          <div className="databox">
+            Databox 2
+          </div>
+          <div className="databox">
+            Databox 3
+          </div>
+        </div>
+        <div className="map">
+          Map
+        </div>
+      </div>
+      <div className="sidebar">
+        sidebar
       </div>
 
     </div>
