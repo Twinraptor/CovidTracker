9f23cf8a63c762a2b1d110286f1cb6f3b1630251
twinraptor <mythosx@hotmail.com>
2021-02-03 20:20:46 -0800

    Setup React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="headerTitle">
+        Covid 19 Tracker
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
